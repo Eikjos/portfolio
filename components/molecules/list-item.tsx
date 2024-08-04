9a44ef5b22573cbddd76aff98da02e6edaf36959
Thomas Hamelin <thomashamelin27@outlook.fr>
2024-08-04 21:29:47 +0200

    design: color

--- components/molecules/list-item.tsx
+++ components/molecules/list-item.tsx
@@ -9,7 +9,7 @@ type ListItemProps = {
 
 const ListItem = ({ title, imageUrl, imageAlt }: ListItemProps) => {
   return (
-    <div className="flex flex-row w-full p-2 gap-4 items-center bg-gradient-to-br from-blue-900 to-green-700 rounded-md hover:animate-pulse">
+    <div className="flex flex-row w-full p-2 gap-4 items-center bg-gradient-to-tr from-gray-800/50 to-gray-700 rounded-md hover:animate-pulse">
       <Image src={imageUrl} width={35} height={35} alt={imageAlt} />
       <span className="text-lg font-sm text-white">{title}</span>
     </div>
