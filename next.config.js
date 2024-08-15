30d923bf53a48fecd7e10ea7afbb53850181465d
Thomas Hamelin <thomashamelin27@outlook.fr>
2024-08-15 11:29:59 +0200

    fix: assets

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   basePath: "/portfolio",
   assetPrefix: "/portfolio/",
-  output: "export",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
 
