6b9d2582af17448da1618b0ff3751c45be987aee
Thomas Hamelin <thomashamelin27@outlook.fr>
2025-06-30 22:32:10 +0200

    fix: ci prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,56 +1,56 @@
-import { Appointment, PrismaClient } from "@prisma/client";
+// import { Appointment, PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma;
+// export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
 
-export async function getExperiences() {
-  return await prisma.experience.findMany({
-    orderBy: [
-      {
-        startDate: "asc",
-      },
-    ],
-  });
-}
+// export async function getExperiences() {
+//   return await prisma.experience.findMany({
+//     orderBy: [
+//       {
+//         startDate: "asc",
+//       },
+//     ],
+//   });
+// }
 
-export async function getDegrees() {
-  return await prisma.graduates.findMany({
-    orderBy: [
-      {
-        startDate: "asc",
-      },
-    ],
-  });
-}
+// export async function getDegrees() {
+//   return await prisma.graduates.findMany({
+//     orderBy: [
+//       {
+//         startDate: "asc",
+//       },
+//     ],
+//   });
+// }
 
-export async function getProjects() {
-  return await prisma.project.findMany({
-    orderBy: [
-      {
-        startDate: "asc",
-      },
-    ],
-  });
-}
+// export async function getProjects() {
+//   return await prisma.project.findMany({
+//     orderBy: [
+//       {
+//         startDate: "asc",
+//       },
+//     ],
+//   });
+// }
 
-export async function getAppointmentByDate(date: Date): Promise<Appointment[]> {
-  return await prisma.appointment.findMany({
-    where: {
-      date: {
-        gte: new Date(date.setHours(0, 0, 0, 0)),
-        lt: new Date(date.setHours(23, 59, 59, 999)),
-      },
-    },
-  });
-}
+// export async function getAppointmentByDate(date: Date): Promise<Appointment[]> {
+//   return await prisma.appointment.findMany({
+//     where: {
+//       date: {
+//         gte: new Date(date.setHours(0, 0, 0, 0)),
+//         lt: new Date(date.setHours(23, 59, 59, 999)),
+//       },
+//     },
+//   });
+// }
